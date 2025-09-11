862121de4464413bb06ff7988b33e8400db8fc1b
rachel199275-pixel <rachel199275@gmail.com>
2025-09-11 14:54:21 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,29 +1,17 @@
-import '../styles/globals.css';
-import { Footer } from '../components/footer';
-import { Header } from '../components/header';
+// app/layout.tsx
+import './globals.css';
 
 export const metadata = {
-    title: {
-        template: '%s | Netlify',
-        default: 'Netlify Starter'
-    }
+  title: 'Rachel 蓓霖',
+  description: '官網設計・SEO關鍵字・網站代管',
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <head>
-                <link rel="icon" href="/favicon.svg" sizes="any" />
-            </head>
-            <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <Header />
-                        <main className="grow">{children}</main>
-                        <Footer />
-                    </div>
-                </div>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="zh-Hant">
+      <body className="bg-white text-slate-900">
+        {children}
+      </body>
+    </html>
+  );
 }
